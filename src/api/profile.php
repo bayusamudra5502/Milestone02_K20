9a482baad10fb07ac268eb21f4bc152cbe0782b7
ignferry <ignasius.ferry01@gmail.com>
2021-08-14 14:26:00 +0700

    Implementasi api profile.php
    implementasi fungsi get_profile dan edit_profile()

--- src/api/profile.php
+++ src/api/profile.php
@@ -10,29 +10,73 @@ function edit_profile($username, $new_data)
 {
   /**
    * Fungsi ini akan mengedit profile sesuai dengan username yang dilewatkan.
-   * Menghasilkan nilai true bila berhasil dna false bila gagal. 
-   * 
+   * Menghasilkan nilai true bila berhasil dna false bila gagal.
+   *
    * Parameter $data adalah array asosiatif yang memiliki keys:
-   * name, email, interest, education, bio, birthday, facebookUrl, 
+   * name, email, interest, education, bio, birthday, facebookUrl,
    * instagramUrl, linkedinUrl, dan photo
    */
 
-  return true;
+   if ($username_exists = run_query("SELECT username FROM tb_accounts
+     WHERE EXISTS username=$username", false)) {
+     $res = false;
+   }
+   elseif (strlen($new_data["nama"]) > 50
+            || strlen($new_data["email"]) > 50
+            || strlen($new_data["interest"]) > 50
+            || strlen($new_data["education"]) > 100) {
+    $res = false;
+   }
+   else {
+    $new_nama = $new_data['nama'];
+    $new_email = $new_data['email'];
+    $new_interest = $new_data['interest'];
+    $new_education = $new_data['education'];
+    $new_bio = $new_data['bio'];
+    $new_birthday = $new_data['birthday'];
+    $new_facebook_url = $new_data['facebook_url'];
+    $new_instagram_url = $new_data['instagram_url'];
+    $new_linkedin_url = $new_data['linkedin_url'];
+    $new_photo = $new_data['photo'];
+
+    run_query("UPDATE tb_accounts SET nama='$new_nama',
+    email='$new_email',
+    interest='$new_interest',
+    education='$new_education',
+    bio='$new_bio',
+    birthday='$new_birthday',
+    facebook_url='$new_facebook_url',
+    instagram_url='$new_instagram_url',
+    linkedin_url='$new_linkedin_url',
+    photo='$new_photo'
+    WHERE username='$username'", false);
+
+    $res = true;
+    }
+    return $res;
 }
 
 function get_profile($username)
 {
   /**
    * Fungsi ini akan mendapatkan profile user dengan nama username.
-   * 
+   *
    * BIla berhasil, Fungsi ini harus menghasilkan array assosiatif dengan keys:
    * name, username, email, password, interest, education, bio, birthday,
    * facebook_url, instagram_url, linkedin_url, dan photo
-   * 
+   *
    * bila gagal, kembalikan nilai null;
    */
-
-  return array("username" => "Yay");
+   if ($username_exists = run_query("SELECT username FROM tb_accounts
+      WHERE EXISTS username=$username", false)){
+     $res = null;
+   }
+   else {
+     $res = run_query("SELECT nama, username, email, password
+       , interest, education, bio, birthday, facebook_url, instagram_url
+       , linkedin_url, photo FROM tb_accounts WHERE username='$username'");
+   }
+   return $res;
 }
 
 header('Content-Type: application/json');
